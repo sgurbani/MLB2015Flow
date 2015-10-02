6a9256d2ced929d8fdae619d08c02736ea09de25
Saumya Gurbani <saumya.gurbani@emory.edu>
2015-10-02 19:45:43 -0400

    Added -TTest and FlowContour

--- data/performWelchTTest.m
+++ data/performWelchTTest.m
@@ -19,6 +19,20 @@ end
 %get top 25 features
 [~, inds] = sort(T,1,'descend');
 
-T25 = inds(1:25);
+T100 = inds(1:100);
+XTrainTop100_Y = [XTrain(T100,:); YTrain'];
+save TrainingData_Top100;
 
-svmstruct = svmtrain( XTrain', YTrain );
+T50 = inds(1:50);
+XTrainTop50_Y = [XTrain(T50,:); YTrain'];
+save TrainingData_Top50;
+
+T25 = inds(1:25);
+XTrainTop25_Y = [XTrain(T25,:); YTrain'];
+save TrainingData_Top25;
+
+XTrain_Y = [XTrain; YTrain'];
+save TrainingData_All;
+
+% svmmodel = fitcsvm(XTrain', YTrain);
+% CV = crossval(svmmodel, 'KFold', 10);
